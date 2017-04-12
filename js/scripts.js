240659555268b1abbecb55d84e6b55e788b7c903
Grady L Shelton <gradyish@gmail.com>
2017-04-12 15:29:09 -0700

    make new method to calculate degrees of freedom

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
+// BUSINESS LOGIC
 
-
+// CONSTRUCTOR TO HOLD ALL CLASS DATA
 function Statistics(){
   this.cohorts = [];
 }
 
+// T-TEST FOR INTRO VS JAVA
 Statistics.prototype.runTTest1 = function() {
   var tTest = 0;
   var introAvg = this.cohorts[0].averageCarbon();
@@ -18,6 +20,8 @@ Statistics.prototype.runTTest1 = function() {
   return tTest;
 }
 
+
+// T-TEST FOR INTRO VS JAVASCRIPT
 Statistics.prototype.runTTest2 = function() {
   var tTest = 0;
   var introAvg = this.cohorts[0].averageCarbon();
@@ -26,12 +30,14 @@ Statistics.prototype.runTTest2 = function() {
   var jSAvg = this.cohorts[2].averageCarbon();
   var jSSDsq = this.cohorts[2].standardDeviation() ** 2;
   var jSN = this.cohorts[2].emissions.length;
+  var dF =
 
   tTest = Math.abs(introAvg - jSAvg) / (Math.sqrt((introSDsq/introN) + (jSSDsq/jSN)));
 
   return tTest;
 }
 
+// T-TEST FOR JAVA VS JAVASCRIPT
 Statistics.prototype.runTTest3 = function() {
   var tTest = 0;
   var javaAvg = this.cohorts[1].averageCarbon();
@@ -46,10 +52,43 @@ Statistics.prototype.runTTest3 = function() {
   return tTest;
 }
 
+// P-VALUE FOR INTRO VS JAVA
+Statistics.prototype.pValue1 = function(){
+  let pValue = 0;
+  var dF = (this.cohorts[0].emissions.length - 1) + (this.cohorts[1].emissions.length -1);
+  var tValue = this.runTTest1();
+  console.log(dF);
+
+  return pValue;
+}
+
+// P-VALUE FOR INTRO VS JAVASCRIPT
+Statistics.prototype.pValue2 = function(){
+  let pValue = 0;
+  var dF = (this.cohorts[0].emissions.length - 1) + (this.cohorts[2].emissions.length -1);
+  var tValue = this.runTTest1();
+  console.log(dF);
+
+  return pValue;
+}
+
+// P-VALUE FOR JAVA VS JAVASCRIPT
+Statistics.prototype.pValue3 = function(){
+  let pValue = 0;
+  var dF = (this.cohorts[1].emissions.length - 1) + (this.cohorts[2].emissions.length -1);
+  var tValue = this.runTTest1();
+  console.log(dF);
+
+  return pValue;
+}
+
+
+// COHORT CONSTRUCTOR WITH EMISSION DATA
 function Cohort(){
   this.emissions = [];
 }
 
+// FIND COHORTS TOTAL CO2
 Cohort.prototype.totalCarbon = function() {
   totalCarbon = 0;
   this.emissions.forEach(function(student){
@@ -57,12 +96,14 @@ Cohort.prototype.totalCarbon = function() {
   });
   return totalCarbon;
 }
-//
+
+// FIND COHORTS AVERAGE CO2
 Cohort.prototype.averageCarbon = function() {
   var average = this.totalCarbon() / this.emissions.length
   return average;
 }
 
+// FIND COHORTS STANDARD DEVIATION
 Cohort.prototype.standardDeviation = function() {
   var sum = 0;
   var avg = this.averageCarbon();
@@ -80,9 +121,10 @@ Cohort.prototype.standardDeviation = function() {
 // USER INTERFACE
 $(function(){
 
+// OBJECT TO STORE ALL THREE COHORTS
   let all3Cohorts = new Statistics();
 
-
+// BUTTON FUNCTION TO DISPLAY INTRO DATA
   $("#introBtn").click(function(){
     let introCohort = new Statistics();
     let introData = new Cohort();
@@ -98,10 +140,10 @@ $(function(){
     $("#introTotal").text(totalC);
     $("#introAverage").text(averageC);
     $("#introStDev").text(stdDev);
-
-
   })
 
+
+// BUTTON FUNCTION TO DISPLAY JAVA DATA
   $("#javaBtn").click(function(){
     let javaCohort = new Statistics();
     let javaData = new Cohort();
@@ -120,6 +162,8 @@ $(function(){
 
   })
 
+
+// BUTTON FUNCTION TO DISPLAY JAVASCRIPT DATA
   $("#jSBtn").click(function(){
     let jSCohort = new Statistics();
     let jSData = new Cohort();
@@ -138,22 +182,31 @@ $(function(){
 
   })
 
+// BUTTON FUNCTION TO CALL T TEST BETWEEN INTRO AND JAVA
   $("#intro-v-java").click(function(){
     tValue = (all3Cohorts.runTTest1()).toFixed(3);
     console.log(tValue);
     $("#intro-java-T-result").text(tValue);
+    pValue = all3Cohorts.pValue1();
+    console.log(pValue);
   })
 
+// BUTTON FUNCTION TO CALL T TEST BETWEEN INTRO AND JAVASCRIPT
   $("#intro-v-jS").click(function(){
     tValue = (all3Cohorts.runTTest2()).toFixed(3);
     console.log(tValue);
     $("#intro-jS-T-result").text(tValue);
+    pValue = all3Cohorts.pValue2();
+    console.log(pValue);
   })
 
+// BUTTON FUNCTION TO CALL T TEST BETWEEN JAVA AND JAVASCRIPT
   $("#java-v-jS").click(function(){
     tValue = (all3Cohorts.runTTest3()).toFixed(3);
     console.log(tValue);
     $("#java-jS-T-result").text(tValue);
+    pValue = all3Cohorts.pValue3();
+    console.log(pValue);
   })
 
 });
